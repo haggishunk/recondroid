9ea530795e7d8e17146856690e6f6751ee1b4d53
haggishunk <travis@pantageo.us>
2019-07-26 14:24:18 -0700

    Removed project def

--- project.tf
+++
@@ -1,9 +0,0 @@
-resource "digitalocean_project" "droneci" {
-  name        = "recondroid"
-  description = "Class project / Educational purposes / playing with drone"
-  purpose     = "Continuous Integration"
-  environment = "Development"
-  resources   = [
-    "${digitalocean_droplet.recondroid.urn}
-  ]
-}
