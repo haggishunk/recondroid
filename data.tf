143b8e843b8c42f2e16583e2a0a752b71b5cde10
haggishunk <travis@pantageo.us>
2019-07-27 19:44:01 -0700

    Using template files and cloudinit configs for droplet user data

--- data.tf
+++ data.tf
@@ -1,3 +1,26 @@
 data "digitalocean_domain" "personal" {
   name = "${var.domain}"
 }
+
+data "template_file" "script" {
+  template                     = "${file("${path.module}/template/drone.sh.tpl")}"
+  vars                         = {
+    DRONE_DATA_VOLUME          = "${var.volume_name}"
+    DRONE_GITHUB_CLIENT_ID     = "${var.github_client_id}"
+    DRONE_GITHUB_CLIENT_SECRET = "${var.github_client_secret}"
+    DRONE_SERVER_HOST          = local.drone_server_host
+    DRONE_RUNNER_CAPACITY      = 2
+    DRONE_LOGS_DEBUG           = "${var.debug}"
+  }
+}
+
+data "template_cloudinit_config" "config" {
+  # digital ocean does not like gzipped or encoded data
+  gzip          = false
+  base64_encode = false
+
+  part {
+    content_type = "text/x-shellscript"
+    content      =  "${data.template_file.script.rendered}"
+  }
+}
--- droplet.tf
+++ droplet.tf
@@ -4,19 +4,8 @@ resource "digitalocean_droplet" "recondroid" {
   image              = "debian-9-x64"
   region             = "sfo2"
   private_networking = true
+  monitoring         = true
   ssh_keys           = ["${var.ssh_key}"]
   tags               = ["ci", "drone"]
-  user_data          = <<-EOF
-    #!/bin/bash
-
-    apt update
-    apt install -y curl
-    curl -sSL https://get.docker.com | sh -
-
-    useradd -m recondroid
-    usermod -aG docker recondroid
-    mkdir /home/recondroid/.ssh
-    cp /root/.ssh/authorized_keys /home/recondroid/.ssh/authorized_keys
-    chown -R recondroid:recondroid /home/recondroid/.ssh
-    EOF
+  user_data          = "${data.template_cloudinit_config.config.rendered}"
 }
