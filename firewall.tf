30d5409f0ffe6b5b530344c6698a6bf116abd71d
haggishunk <travis@pantageo.us>
2019-07-27 19:45:12 -0700

    Removed vestigial outbound web rules from ssh firewall

--- firewall.tf
+++ firewall.tf
@@ -8,18 +8,6 @@ resource "digitalocean_firewall" "ssh" {
     port_range       = "22"
     source_addresses = ["0.0.0.0/0"]
   }
-
-  outbound_rule {
-    protocol              = "tcp"
-    port_range            = "80"
-    destination_addresses = ["0.0.0.0/0"]
-  }
-
-  outbound_rule {
-    protocol              = "tcp"
-    port_range            = "443"
-    destination_addresses = ["0.0.0.0/0"]
-  }
 }
 
 resource "digitalocean_firewall" "dns" {
